70201ce314aaa3eedebd2653e94a22b28afb57b8
Kaito <mail.kaito03@gmail.com>
2020-01-05 15:12:09 +0900

    add serializer for user

---
+++ user/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'email', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
