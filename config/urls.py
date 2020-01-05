bf0fb9a8531fd6ba170083f3c1ed922adc778442
Kaito <mail.kaito03@gmail.com>
2020-01-05 17:27:52 +0900

    add auth & api routet

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_views
-from api.urls import router
+from api.urls import router as api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', auth_views.obtain_auth_token),  # TOKEN取得用
-    path('api/', include(router.urls)),  # api
+    path('api-token-auth/', auth_views.obtain_auth_token),  # POST: username, password => TOKEN
+    path('api/', include(api_router.urls)),
 ]
