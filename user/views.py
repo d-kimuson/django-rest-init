6b44c1d8f5525d75028a91b531557b7bcdc52a76
Kaito <mail.kaito03@gmail.com>
2020-01-05 15:23:11 +0900

    define user view set

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+# from rest_framework.response import Response
+# from rest_framework.decorators import action
+# from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .serializer import UserSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
